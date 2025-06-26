71ad87a0cdd6e68849724f40f00e99d0904b7e49
Daniel Solomon <danielsolomon@mailfence.com>
2025-06-26 10:48:13 +0100

    Fixing issue with foreign keys when creating tables.

--- dags/process_users.py
+++ dags/process_users.py
@@ -3,110 +3,83 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.standard.operators.python import PythonOperator
+import pandas as pd
 
 
 @dag
 def user_processing():
     
-    create_gender_table = SQLExecuteQueryOperator(
-        task_id="create_gender_table",
+    create_tables = SQLExecuteQueryOperator(
+        task_id="create_tables",
         conn_id="postgres",
         sql="CREATE TABLE IF NOT EXISTS gender (" \
             "   gender_id SERIAL PRIMARY KEY," \
             "   gender VARCHAR(50)" \
             ")" \
             ";"
-    )
-            
-    create_title_table = SQLExecuteQueryOperator(
-        task_id="create_title_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS title (" \
-            "   title_id SERIAL PRIMARY KEY," \
-            "   title VARCHAR(50)" \
+            "CREATE TABLE IF NOT EXISTS title (" \
+                    "   title_id SERIAL PRIMARY KEY," \
+                    "   title VARCHAR(50)" \
             ")" \
             ";"
-    )
-            
-    create_country_table = SQLExecuteQueryOperator(
-        task_id="create_country_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS country (" \
-            "   country_id SERIAL PRIMARY KEY," \
-            "   country VARCHAR(50)" \
+            "CREATE TABLE IF NOT EXISTS country (" \
+                    "   country_id SERIAL PRIMARY KEY," \
+                    "   country VARCHAR(50)" \
             ")" \
             ";"
-    )
-
-    create_state_table = SQLExecuteQueryOperator(
-        task_id="create_state_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS state (" \
-            "   state_id SERIAL PRIMARY KEY," \
-            "   state VARCHAR(50)," \
-            "   CONSTRAINT fk_country FOREIGN KEY (country_id)"
-            "   REFERENCES country(country_id)" \
+            "CREATE TABLE IF NOT EXISTS state (" \
+                    "   state_id SERIAL PRIMARY KEY," \
+                    "   state VARCHAR(50)," \
+                    "   country_id INT," \
+                    "   CONSTRAINT fk_country FOREIGN KEY (country_id)"
+                    "   REFERENCES country(country_id)" \
             ")" \
             ";"
-    )
-
-    create_city_table = SQLExecuteQueryOperator(
-        task_id="create_city_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS city (" \
-            "   city_id SERIAL PRIMARY KEY," \
-            "   city VARCHAR(50)," \
-            "   CONSTRAINT fk_state FOREIGN KEY (state_id)"
-            "   REFERENCES state(state_id)" \
+            "CREATE TABLE IF NOT EXISTS city (" \
+                    "   city_id SERIAL PRIMARY KEY," \
+                    "   city VARCHAR(50)," \
+                    "   state_id INT," \
+                    "   CONSTRAINT fk_state FOREIGN KEY (state_id)"
+                    "   REFERENCES state(state_id)" \
             ")" \
             ";"
-    )
-
-    create_street_table = SQLExecuteQueryOperator(
-        task_id="create_street_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS street (" \
-            "   street_id SERIAL PRIMARY KEY," \
-            "   street VARCHAR(250)," \
-            "   CONSTRAINT fk_city FOREIGN KEY (city_id)"
-            "   REFERENCES city(city_id)" \
+            "CREATE TABLE IF NOT EXISTS street (" \
+                    "   street_id SERIAL PRIMARY KEY," \
+                    "   street VARCHAR(250)," \
+                    "   city_id INT," \
+                    "   CONSTRAINT fk_city FOREIGN KEY (city_id)"
+                    "   REFERENCES city(city_id)" \
             ")" \
             ";"
-    )
-
-    create_address_table = SQLExecuteQueryOperator(
-        task_id="create_address_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS address (" \
-            "   address_id SERIAL PRIMARY KEY," \
-            "   street_number INT," \
-            "   CONSTRAINT fk_street FOREIGN KEY (street_id)"
-            "   REFERENCES street(street_id)," \
-            "   postcode VARCHAR(100)" \
+            "CREATE TABLE IF NOT EXISTS address (" \
+                        "   address_id SERIAL PRIMARY KEY," \
+                        "   street_number INT," \
+                        "   street_id INT," \
+                        "   CONSTRAINT fk_street FOREIGN KEY (street_id)"
+                        "   REFERENCES street(street_id)," \
+                        "   postcode VARCHAR(100)" \
             ")" \
             ";"
-    )
-
-    create_user_table = SQLExecuteQueryOperator(
-        task_id="create_user_table",
-        conn_id="postgres",
-        sql="CREATE TABLE IF NOT EXISTS user_table (" \
-            "   user_id SERIAL PRIMARY KEY," \
-            "   CONSTRAINT fk_title FOREIGN KEY (title_id)"
-            "   REFERENCES title(title_id)," \
-            "   first_name VARCHAR(200)," \
-            "   last_name VARCHAR(200)," \
-            "   CONSTRAINT fk_gender FOREIGN KEY (gender_id)"
-            "   REFERENCES gender(gender_id)," \
-            "   email VARCHAR(200)," \
-            "   phone VARCHAR(200)," \
-            "   cell_phone VARCHAR(200)," \
-            "   username VARCHAR(300)," \
-            "   password VARCHAR(300)," \
-            "   CONSTRAINT fk_address FOREIGN KEY (address_id)"
-            "   REFERENCES address(address_id)," \
-            "   date_of_birth DATE," \
-            "   date_registered DATE" \
+            "CREATE TABLE IF NOT EXISTS user_table (" \
+                        "   user_id SERIAL PRIMARY KEY," \
+                        "   title_id INT," \
+                        "   CONSTRAINT fk_title FOREIGN KEY (title_id)"
+                        "   REFERENCES title(title_id)," \
+                        "   first_name VARCHAR(200)," \
+                        "   last_name VARCHAR(200)," \
+                        "   gender_id INT," \
+                        "   CONSTRAINT fk_gender FOREIGN KEY (gender_id)"
+                        "   REFERENCES gender(gender_id)," \
+                        "   email VARCHAR(200)," \
+                        "   phone VARCHAR(200)," \
+                        "   cell_phone VARCHAR(200)," \
+                        "   username VARCHAR(300)," \
+                        "   password VARCHAR(300)," \
+                        "   address_id INT," \
+                        "   CONSTRAINT fk_address FOREIGN KEY (address_id)"
+                        "   REFERENCES address(address_id)," \
+                        "   date_of_birth DATE," \
+                        "   date_registered DATE" \
             ")" \
             ";"
     )
@@ -166,13 +139,13 @@ def user_processing():
                 # Get the field name if nested
                 elif isinstance(field, list):
                     last_index = len(field) - 1
-                    name_of_field = field[last_index]
+                    name_of_field = f"{field[last_index - 1]}_{field[last_index]}"
 
-                    # Get the field name if nested once
+                    # Get the field value if nested once
                     if len(field) == 2:
                         user_dict[name_of_field] = user[field[0]][field[1]]
 
-                    # Get the field name if nested twice
+                    # Get the field value if nested twice
                     elif len(field) == 3:
                         user_dict[name_of_field] = user[field[0]][field[1]][field[2]]
 
@@ -181,7 +154,12 @@ def user_processing():
         print(users_details)
         return users_details
 
+
+    @task
+    def users_to_dfs(users_details):
+        pass
+
     
-    extract_users_details(create_gender_table >> create_title_table >> create_country_table >> create_state_table >> create_city_table >> create_street_table >> create_address_table >> create_user_table >> is_api_available())
+    extract_users_details(create_tables >> is_api_available())
 
 user_processing()
